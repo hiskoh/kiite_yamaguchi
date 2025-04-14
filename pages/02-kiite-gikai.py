3f6e45eb3567cd9340ce076159fd8f066d8bcd7c
hiskoh <his.koh3.biz@gmail.com>
2025-04-14 22:31:50 +0900

    Update and rename kiite-gikai.py to 02-kiite-gikai.py

--- pages/02-kiite-gikai.py
+++ pages/02-kiite-gikai.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_title="きいてギカイやまぐち", layout="wide")
+st.set_page_config(page_title="きいてギカイやまぐち", layout="wide", page_icon="📜)
 st.title("きいてギカイやまぐち")
 st.write("こちらは議会のやりとりから答える“勝手議会”モードです。")
